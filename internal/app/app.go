d9a72e783c7cb11987581a268c636d08d19f36b4
pinnakarn-k <pinnakarn.k@gmail.com>
2026-06-08 13:33:04 +0700

    fc

--- internal/app/app.go
+++ internal/app/app.go
@@ -6,10 +6,17 @@ import (
 	"log/slog"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func New(log *slog.Logger) *fiber.App {
 	fiberApp := fiber.New()
+
+	fiberApp.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowMethods: "GET,POST,PUT,PATCH,DELETE,OPTIONS",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization, X-Request-Id",
+	}))
 
 	fiberApp.Use(middleware.RequestID())
 	fiberApp.Use(middleware.Logger(log))
