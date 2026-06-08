b4d1745ab14caf8fb6d715db180f71f5502d9155
pinnakarn-k <pinnakarn.k@gmail.com>
2026-06-08 09:36:10 +0700

    fm

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -2,15 +2,37 @@ package validator
 
 import (
 	"errors"
+	"reflect"
+	"strings"
 
 	"backend/internal/apperror"
 
 	govalidator "github.com/go-playground/validator/v10"
 )
 
-var validate = govalidator.New()
+var validate = newValidator()
 
-func ValidateStruct(input any) error {
+func newValidator() *govalidator.Validate {
+	v := govalidator.New()
+
+	v.RegisterTagNameFunc(func(field reflect.StructField) string {
+		name := strings.SplitN(
+			field.Tag.Get("json"),
+			",",
+			2,
+		)[0]
+
+		if name == "-" {
+			return ""
+		}
+
+		return name
+	})
+
+	return v
+}
+
+func Validate(input any) error {
 	if err := validate.Struct(input); err != nil {
 		var validationErrors govalidator.ValidationErrors
 
@@ -37,12 +59,16 @@ func messageOf(err govalidator.FieldError) string {
 	switch err.Tag() {
 	case "required":
 		return err.Field() + " is required"
+
 	case "email":
 		return err.Field() + " must be a valid email"
+
 	case "min":
 		return err.Field() + " must be at least " + err.Param()
+
 	case "max":
 		return err.Field() + " must be at most " + err.Param()
+
 	default:
 		return err.Field() + " is invalid"
 	}
